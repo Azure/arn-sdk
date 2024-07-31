c5e001f98ca4f464aed5c9371987593752a91d76
John Doak <jdoak@ElephantInTheRoom.local>
2024-07-31 13:02:38 -0700

    Removing APIVersion type

--- models/version/version.go
+++ models/version/version.go
@@ -9,9 +9,3 @@ const (
 	// V3 is the schema version 3.
 	V3 Schema = "3.0"
 )
-
-// API is the API version of the SDK.
-type API string
-
-// API2020 is the API version 2020-05-01.
-const API2020 API = "2020-05-01"
