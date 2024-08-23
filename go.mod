c029a142960e93edd93305525d5b4374445ca16f
John Doak <jdoak@ElephantInTheRoom.local>
2024-08-23 11:25:22 -0700

    Adds deflate compression and fixes an http buffer bug

--- go.mod
+++ go.mod
@@ -8,15 +8,20 @@ require (
 	github.com/Azure/retry v0.0.0-20240325164105-70e16f388626
 	github.com/go-json-experiment/json v0.0.0-20240524174822-2d9f40f7385b
 	github.com/google/uuid v1.6.0
+	github.com/gostdlib/concurrency v0.0.0-20240403195145-a5b82e576be2
+	github.com/johnsiilver/pools v0.0.0-20221216174331-e94bf08bc72b
 	github.com/kylelemons/godebug v1.1.0
 )
 
 require (
 	github.com/Azure/azure-sdk-for-go/sdk/internal v1.10.0 // indirect
+	github.com/gostdlib/internals v0.0.0-20240319155855-57c259c0554f // indirect
 	github.com/jedib0t/go-pretty/v6 v6.5.6 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/sanity-io/litter v1.5.5 // indirect
+	go.opentelemetry.io/otel v1.24.0 // indirect
+	go.opentelemetry.io/otel/trace v1.24.0 // indirect
 	golang.org/x/net v0.27.0 // indirect
 	golang.org/x/sys v0.22.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
---
+++ internal/conn/http/defalte_test.go
@@ -0,0 +1,164 @@
+package http
+
+import (
+	"bytes"
+	"compress/flate"
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/Azure/arn-sdk/internal/build"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
+	"github.com/go-json-experiment/json"
+	"github.com/google/uuid"
+	"github.com/gostdlib/concurrency/goroutines/limited"
+	"github.com/gostdlib/concurrency/prim/wait"
+)
+
+type flateData struct {
+	Num int
+	ID  string
+}
+
+type httpHandler struct {
+	results []flateData
+}
+
+// handleDeflateRequest is an HTTP handler that processes requests with deflate-encoded bodies.
+func (h *httpHandler) handleDeflateRequest(w http.ResponseWriter, r *http.Request) {
+	// Check if the Content-Encoding is deflate
+	if r.Header.Get("Content-Encoding") == "deflate" {
+		// Wrap the request body in a flate.Reader to decompress it
+		deflateReader := flate.NewReader(r.Body)
+		defer deflateReader.Close()
+
+		// Read the decompressed data
+		decompressedBody, err := io.ReadAll(deflateReader)
+		if err != nil {
+			http.Error(w, "Failed to read deflated body", http.StatusBadRequest)
+			log.Println(err)
+			return
+		}
+
+		var f flateData
+		if err := json.Unmarshal(decompressedBody, &f); err != nil {
+			http.Error(w, "Failed to unmarshal deflated body", http.StatusBadRequest)
+			log.Println(err)
+			return
+		}
+
+		h.results[f.Num] = f
+
+		// Send a response
+		w.Write([]byte("Successfully processed deflated request"))
+	} else {
+		// If not deflate-encoded, handle normally
+		w.Write([]byte("Request is not deflate-encoded"))
+	}
+}
+
+func TestDeflate(t *testing.T) {
+	t.Parallel()
+
+	data := make([]flateData, 0, 1000)
+
+	for i := 0; i < 1000; i++ {
+		u, err := uuid.NewV7()
+		if err != nil {
+			panic(err)
+		}
+
+		d := flateData{
+			Num: i,
+			ID:  u.String(),
+		}
+		data = append(data, d)
+	}
+
+	handler := &httpHandler{results: make([]flateData, len(data))}
+	// Set up the HTTP route
+	http.HandleFunc("/deflate", handler.handleDeflateRequest)
+
+	listener, err := net.Listen("tcp", ":0")
+	if err != nil {
+		panic(err)
+	}
+
+	go func() {
+		if err := http.Serve(listener, nil); err != nil {
+			fmt.Printf("Failed to start server: %v\n", err)
+		}
+	}()
+	time.Sleep(1 * time.Second)
+
+	endpoint := fmt.Sprintf("http://localhost:%v/deflate", listener.Addr().(*net.TCPAddr).Port)
+
+	plOpts := runtime.PipelineOptions{
+		PerRetry: []policy.Policy{
+			newFlatTransport(),
+		},
+	}
+	azclient, err := azcore.NewClient("arn.Client", build.Version, plOpts, &policy.ClientOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	pool, err := limited.New("test", 100)
+
+	wg := wait.Group{
+		Pool: pool,
+	}
+
+	var count int
+	for _, d := range data {
+		d := d
+		count++
+		wg.Go(
+			context.Background(),
+			func(ctx context.Context) error {
+				b, err := json.Marshal(d)
+				if err != nil {
+					return err
+				}
+
+				req, err := runtime.NewRequest(context.Background(), http.MethodPost, endpoint)
+				if err != nil {
+					return err
+				}
+				req.Raw().Header["Accept"] = appJSON
+				req.SetBody(rsc{bytes.NewReader(b)}, "application/json")
+
+				// Send the event to the ARN service.
+				resp, err := azclient.Pipeline().Do(req)
+				if err != nil {
+					return err
+				}
+				if resp.StatusCode != http.StatusOK {
+					return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+				}
+				return nil
+			},
+		)
+	}
+
+	if err := wg.Wait(context.Background()); err != nil {
+		t.Fatal(err)
+	}
+
+	if len(handler.results) != len(data) {
+		t.Fatalf("TestDeflate: expected %d results, got %d", len(data), len(handler.results))
+	}
+
+	for _, result := range handler.results {
+		if result.ID != data[result.Num].ID {
+			t.Fatalf("TestDeflate: expected ID %s, got %s", data[result.Num].ID, result.ID)
+		}
+	}
+}
