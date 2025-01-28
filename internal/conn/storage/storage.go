e843556452297bc7cf0892ddbed24c31f4c4e65f
Shoshana <shmalfat@microsoft.com>
2025-01-28 12:03:36 -0800

    set to debug

--- internal/conn/storage/storage.go
+++ internal/conn/storage/storage.go
@@ -246,7 +246,7 @@ func (c *Client) upload(ctx context.Context, args uploadArgs) (*url.URL, error) 
 		BlobName:      args.bName,
 	}
 
-	c.log.Info(fmt.Sprintf("Uploading to blob. Container: %s, Blob: %s", args.cName, args.bName))
+	c.log.Debug(fmt.Sprintf("Uploading to blob. Container: %s, Blob: %s", args.cName, args.bName))
 
 	enc, err := c.signParams(sigVals, cred)
 	if err != nil {
